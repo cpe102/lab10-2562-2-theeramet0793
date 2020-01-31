a1a1a050c08fab9f3ba430f926c94b94e7f9bc61
theeramet0793 <theerametmetha@gmail.com>
2020-01-31 15:06:58 +0700

    commit-1

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,2 +1,41 @@
 // cout << "Press Enter 3 times to reveal your future.";
 // cout << "You will get A in this 261102.";
+#include<iostream>
+#include<string>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+int main()
+{
+    string a,b,c;
+    int num;
+    cout << "Press Enter 3 times to reveal your future.\n";
+    getline(cin,a);
+    getline(cin,b);
+    getline(cin,c);
+    srand(time(0));
+    num=rand()%9;
+
+    if(num==0)
+    cout << "You will get A in this 261102.";
+    else if(num==1)
+    cout << "You will get B+ in this 261102.";
+    else if(num==2)
+    cout << "You will get B in this 261102.";
+    else if(num==3)
+    cout << "You will get C+ in this 261102.";
+    else if(num==4)
+    cout << "You will get C in this 261102.";
+    else if(num==5)
+    cout << "You 6ill get D+ in this 261102.";
+    else if(num==6)
+    cout << "You will get D in this 261102.";
+    else if(num==7)
+    cout << "You will get F in this 261102.";
+    else 
+    cout << "You will get W in this 261102.";
+    
+    
+    return 0;
+}
+
